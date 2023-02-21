34b24f78eb601e7e873c72a3cce999dfba58eb4b
Casto Gil <casto782@gmail.com>
2023-02-21 00:32:05 -0300

    mongo

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
 import myHttpServer from "./services/server.js";
+import dotenv from "dotenv";
+dotenv.config();
+const uri = process.env.MONGO_URI;
 
 //Servidor Escuchando//
 const PORT = process.env.PORT || 8080;
 myHttpServer.listen(PORT, () => console.log("server listening on port", PORT));
 
 //conectamos a la base de datos//
-mongoose.connect('mongodb+srv://ecommerce:coderhouse@cluster0.xc7i2xr.mongodb.net/ecommerce?retryWrites=true&w=majority', (error)=>{
-    if(error){
-        console.log("cannot connect to database"+error)
-        process.exit()
+mongoose.set("strictQuery", false);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log("Connected to database"),
+  (error) => {
+    if (error) {
+      console.log("cannot connect to database" + error);
+      process.exit();
     }
-})
+  };
